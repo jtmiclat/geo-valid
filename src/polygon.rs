e7fc60b89c481108a172a1d990e375b474793ec8
Jt Miclat <jtmiclat@gmail.com>
2023-02-18 21:42:44 +0800

    Add simple self intersection

--- src/polygon.rs
+++ src/polygon.rs
@@ -1,16 +1,52 @@
-use geo_types::{Point, Polygon};
+use geo::{
+    algorithm::line_intersection::line_intersection, Coord, GeoFloat, Line, LineIntersection,
+};
+use geo_types::Polygon;
 
 use super::linestring::LineStringValidationExt;
+
+pub trait LineIntersectionExt {
+    fn single_point(&self) -> Option<Coord>;
+}
+impl<F: GeoFloat> LineIntersectionExt for LineIntersection<F> {
+    fn single_point(&self) -> Option<Coord> {
+        match self {
+            Self::Collinear { .. } => None,
+            Self::SinglePoint { intersection, .. } => Some(Coord::from((
+                intersection.x.to_f64()?,
+                intersection.y.to_f64()?,
+            ))),
+        }
+    }
+}
 
 pub trait PolygonValidationExt {
     fn all_points_are_valid(&self) -> bool;
     fn rings_closed(&self) -> bool;
     fn rings_size(&self) -> bool;
     fn is_valid(&self) -> bool;
-    fn get_self_intersection(&self) -> Option<Point>;
+    fn get_self_intersection(&self) -> Option<Coord>;
 }
 impl PolygonValidationExt for Polygon {
-    fn get_self_intersection(&self) -> Option<Point> {
+    fn get_self_intersection(&self) -> Option<Coord> {
+        let exterior = self.exterior();
+        let lines: Vec<Line> = exterior.lines().collect();
+        for line in lines.clone() {
+            for line2 in lines.clone() {
+                //skip if comparing the same part
+                if line == line2 {
+                    continue;
+                }
+                if let Some(intersection) = line_intersection(line, line2) {
+                    if intersection.is_proper() {
+                        if let Some(point) = intersection.single_point() {
+                            return Some(point);
+                        }
+                    }
+                }
+            }
+        }
+
         None
     }
     fn all_points_are_valid(&self) -> bool {
@@ -61,6 +97,7 @@ impl PolygonValidationExt for Polygon {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use geo::coord;
     use geo_types::LineString;
     #[test]
     fn all_points_are_valid() {
@@ -73,6 +110,6 @@ mod tests {
             LineString::from(vec![(0., 0.), (2., 2.), (2., 0.), (0., 2.)]),
             vec![],
         );
-        assert_eq!(polygon.get_self_intersection(), Some(Point::new(1.0, 1.0)))
+        assert_eq!(polygon.get_self_intersection(), Some(coord! {x:1.0, y:1.0}))
     }
 }
