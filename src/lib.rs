4c3dfc0c8d7b10bebf70045812c9b4a53d918b5e
Jt Miclat <jtmiclat@gmail.com>
2023-02-24 21:18:18 +0800

    Add polygon at top level

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use geo_types::geometry::Geometry;
 use linestring::validate_linestring;
 use point::validate_point;
+use polygon::validate_polygon;
 
 pub mod coord;
 pub mod linestring;
@@ -12,6 +13,7 @@ pub fn validate(geom: Geometry) -> validator::Validation {
     match geom {
         Geometry::Point(g) => validate_point(g),
         Geometry::LineString(g) => validate_linestring(g),
+        Geometry::Polygon(g) => validate_polygon(g),
         _ => validator::Validation {
             is_valid: true,
             errors: vec![],
